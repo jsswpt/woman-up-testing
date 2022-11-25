7656cd5f590a78ac7ed2292aaf7c15f840fb5d48
jsswpt <paiusov.d@yandex.ru>
2022-11-25 13:33:29 +0500

    Хок для объединения всех провайдеров

--- src/app/providers/with-providers.ts
+++ src/app/providers/with-providers.ts
@@ -1,8 +1,7 @@
 import compose from "compose-function";
 import { withFirebase } from "./with-firebase";
-import { withRouter } from "./with-router";
 
 /**
  * композирует хоки в один
  */
-export const withProviders = compose(withFirebase, withRouter);
+export const withProviders = compose(withFirebase);
