cab78db39e1ba1c012a9c4d728f788d521eb84a5
jsswpt <paiusov.d@yandex.ru>
2022-11-25 09:07:26 +0500

    initial

---
+++ src/shared/api/firebase/config.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getEnvVar } from "shared/lib/get-env-var/get-env-var";
+
+const firebaseConfig = {
+  apiKey: getEnvVar("REACT_APP_API_KEY"),
+  authDomain: getEnvVar("REACT_APP_AUTH_DOMAIN"),
+  projectId: getEnvVar("REACT_APP_PROJECT_ID"),
+  storageBucket: getEnvVar("REACT_APP_STORAGE_BUCKET"),
+  messagingSenderId: getEnvVar("REACT_APP_MESSAGING_SENDER_ID"),
+  appId: getEnvVar("REACT_APP_MESSAGING_APP_ID"),
+};
+
+export const app = initializeApp(firebaseConfig);
+export const firestore = getFirestore(app);
