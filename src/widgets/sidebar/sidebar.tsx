3f73a4538c8d60c96a9000e16cbce2fa569861d4
jsswpt <paiusov.d@yandex.ru>
2022-11-26 23:16:22 +0500

    Небольшие фиксы сайдбара

--- src/widgets/sidebar/sidebar.tsx
+++ src/widgets/sidebar/sidebar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Suspense } from "react";
 import classNames from "classnames";
 import { Logo } from "shared/assets/svg/media/logo";
 import { useScreen } from "shared/lib/useScreen";
@@ -7,6 +7,15 @@ import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 import st from "./styles.module.scss";
 import { useAnimation } from "shared/lib/useAnimation";
 import { IconButton } from "shared/ui/icon-button/icon-button";
+import { Link } from "react-router-dom";
+import { routePaths } from "shared/api/internal/consts/route-paths";
+
+const SidebarCategoriesList = React.lazy(
+  () =>
+    import(
+      "entities/categories/ui/sidebar-categories-list/sidebar-categories-list"
+    )
+);
 
 const Sidebar = () => {
   const screen = useScreen();
@@ -26,13 +35,16 @@ const Sidebar = () => {
         className={classNames(st.sidebar, {
           [st.closing]: state.isInTransition && state.isOpen,
           [st.opening]: state.isInTransition && !state.isOpen,
-          [st.closed]: !state.isInTransition && !state.isOpen,
+          [st.closed]:
+            !state.isInTransition && !state.isOpen && screen !== "lg",
           [st.opened]: !state.isInTransition && state.isOpen,
         })}
       >
         <div className={st.sidebar_top}>
           <div className={st.logo_wrap}>
-            <Logo />
+            <Link to={routePaths.publicNavigation.HOME}>
+              <Logo />
+            </Link>
           </div>
           {screen !== "lg" && (
             <IconButton onClick={() => state.toggleAnimation(false)}>
@@ -41,10 +53,12 @@ const Sidebar = () => {
           )}
         </div>
         <div className={st.sidebar_middle}>
-          <SidebarBlockLayout title="Категории">категории</SidebarBlockLayout>
-          <SidebarBlockLayout title="Условный блок">
-            лист категорий
+          <SidebarBlockLayout title="Категории">
+            <Suspense>
+              <SidebarCategoriesList />
+            </Suspense>
           </SidebarBlockLayout>
+          <SidebarBlockLayout title="Условный блок"></SidebarBlockLayout>
           <SidebarBlockLayout title="Ещё условный блок">
             условный блок
           </SidebarBlockLayout>
