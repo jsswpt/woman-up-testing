cd6310a77f06e2d6ffe30b1fc92b3910a2f398f5
jsswpt <paiusov.d@yandex.ru>
2022-11-27 09:21:51 +0500

    Добавлено модальное окно

---
+++ src/shared/ui/modal/modal.tsx
@@ -0,0 +1,61 @@
+import classNames from "classnames";
+import React, { useEffect, useMemo } from "react";
+import ReactDOM from "react-dom";
+import { useAnimation } from "shared/lib/useAnimation";
+import st from "./styles.module.scss";
+
+type ModalProps = {
+  children: React.ReactNode;
+  isOpen: boolean;
+  className?: any;
+  onClose: () => void;
+};
+
+export const Modal = (props: ModalProps) => {
+  const animation = useAnimation(props.isOpen, 800);
+  const className = props.className || "";
+
+  useEffect(() => {
+    if (props.isOpen) {
+      animation.toggleAnimation(true);
+    } else if (props.isOpen !== animation.isOpen) {
+      animation.toggleAnimation(false);
+    }
+  }, [props.isOpen]);
+
+  //   элемент модального окна
+  const modal = useMemo(() => {
+    const el = document.createElement("div");
+    el.className = classNames(st.modal, className);
+    el.addEventListener("click", () => {
+      props.onClose();
+      animation.toggleAnimation(false);
+    });
+
+    return el;
+  }, []);
+
+  //   отвечает только за аоткрыто / закрыто
+  useEffect(() => {
+    if (!animation.isInTransition && !animation.isOpen) {
+      modal.remove();
+    } else {
+      document.body.appendChild(modal);
+    }
+  }, [animation]);
+
+  //   отвечает за анимацию
+  useEffect(() => {
+    if (animation.isInTransition && !animation.isOpen) {
+      modal.className = classNames(st.modal, className, st.modal_opening);
+    } else if (animation.isInTransition && animation.isOpen) {
+      modal.className = classNames(st.modal, className, st.modal_closing);
+    } else if (!animation.isInTransition && animation.isOpen) {
+      modal.className = classNames(st.modal, className, st.modal_opened);
+    } else if (!animation.isInTransition && !animation.isOpen) {
+      modal.className = classNames(st.modal, className, st.modal_closed);
+    }
+  }, [animation]);
+
+  return props.isOpen ? ReactDOM.createPortal(props.children, modal) : <></>;
+};
